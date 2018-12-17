06a75440b17f180d17e6e804b98c0073055ad855
Wenyi An <anwenyi@hotmail.com>
2018-12-16 20:50:00 -0600

    Refactor and clean code

--- src/src/Solution.java
+++ src/src/Solution.java
@@ -1,32 +1,68 @@
 package src;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 /**
 * The String calculator program can calculate the sum of all the numbers in a string
 *
 * @version 1.0
-* @since   2018-12-16
+* @since   2018-12-16 
 */
-public class Solution {
+public final class Solution {
 	
-	private static final String NEW_LINE = "\n";
-	private static final String SPLIT = ",";
+	private static final String DELIMITER_SPLIT_NEW_LINE = "\n";
+	private static final String MULTIPLE_DELIMITER_SPLIT = ",";
+	
+	 private Solution() {
+	        throw new AssertionError("Private constructor is visited.");
+	    }
+	 
+	/**
+	   * This is the method which find out all the delimiters
+	   * @param inputString Input string
+	   * @return delimiters A set that has all the delimiters
+	   */
+	private static String determineDelimiter(final String inputString){
+		StringBuilder delimiters = new StringBuilder();
+		delimiters.append(MULTIPLE_DELIMITER_SPLIT);
+		if(inputString.startsWith("//")){
+				delimiters.append("|");
+			 	String delimiterStr = inputString.substring(2).split(DELIMITER_SPLIT_NEW_LINE)[0];
+		        
+		        for(char eachSingleDelimiter : delimiterStr.toCharArray()){
+		            if(eachSingleDelimiter == ',')
+		                delimiters.append("|");
+		            else{
+		                delimiters.append("\\");
+		                delimiters.append(eachSingleDelimiter);
+		            }
+		        }
+		}
+		return delimiters.toString();
+	}
 	
 	/**
 	   * This is the method which calculate the sum
-	   * @param delimiter
+	   * @param delimiterSet
 	   * @param inputString Input string
-	   * @param s 
 	   * @return result The sum of all the numbers in the inputString
 	   */
-	public int calculateResult(final String delimiter,final String inputString){
+	private static int calculateResult(final String delimiterSet,final String inputString){
 		int result = 0;
-		for(String number:inputString.split(delimiter.toString())){
+		System.out.println("delimiterSet "+delimiterSet);
+		List<Integer> negativeList = new ArrayList<Integer>();
+		for(String number:inputString.split(delimiterSet.toString())){
 			if(number.trim().length()>0){
 				int num = Integer.parseInt(number.trim());
-				result += num;
+				if (num<1000 && num>=0)
+					result += num;
+				else if (num<0)
+					negativeList.add(num);
 			}
 		}
+		if(negativeList.size()>0) throw new IllegalArgumentException("Negatives not allowed"+Arrays.toString(negativeList.toArray()));
 		return result;
 	}
 	
@@ -35,9 +71,13 @@ public class Solution {
 	   * @param inputString Input string
 	   * @return calculateResult function The sum of all the numbers in the inputString
 	   */
-	public int stringCalculator(final String inputString){
+	public static int stringCalculator(final String inputString){
 		if(Objects.isNull(inputString) || inputString.length() == 0) return 0;
-		return calculateResult(SPLIT+"|"+NEW_LINE, inputString);
+		String expression = inputString;
+		if (inputString.startsWith("//")) {
+			expression = inputString.substring(inputString.indexOf(DELIMITER_SPLIT_NEW_LINE) + DELIMITER_SPLIT_NEW_LINE.length());
+		}
+		return calculateResult(determineDelimiter(inputString), expression);
 	}
 
 }
--- src/test/UnitTestsForStringCalculator.java
+++ src/test/UnitTestsForStringCalculator.java
@@ -1,57 +1,120 @@
 package test;
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import src.Solution;
 
 public class UnitTestsForStringCalculator{
-	private Solution solution;
-	@Before
-	public void init() {
-		solution = new Solution();
-	}
-
+	@Rule
+	public ExpectedException expectedEx = ExpectedException.none();
+	
 	@Test
 	public void inputStringIsNull() throws Exception{
-		assertEquals(0, solution.stringCalculator(null));
+		assertEquals(0, Solution.stringCalculator(null));
 	}
 	
 	@Test
 	public void inputStringEmptyString() throws Exception{
-		assertEquals(0, solution.stringCalculator(""));
+		assertEquals(0, Solution.stringCalculator(""));
 	}
 	
 	@Test
 	public void inputStringOneNumberString() throws Exception{
-		assertEquals(90, solution.stringCalculator("90"));
+		assertEquals(90, Solution.stringCalculator("90"));
 	}
 	
 	@Test
 	public void inputStringWithComma() throws Exception{
-		assertEquals(6, solution.stringCalculator("1,2,3"));
+		assertEquals(6, Solution.stringCalculator("1,2,3"));
 	}
 	
 	@Test
 	public void inputStringWithCommaSpace() throws Exception{
-		assertEquals(6, solution.stringCalculator("1 , 2 ,3 "));
+		assertEquals(6, Solution.stringCalculator("1 , 2 ,3 "));
 	}
 	
 	@Test
 	public void inputStringWithCommaNewLine() throws Exception{
-		assertEquals(6, solution.stringCalculator("1\n,2,3"));
+		assertEquals(6, Solution.stringCalculator("1\n,2,3"));
 	}
 	
 	@Test
 	public void inputStringWithCommaNewLineSpace() throws Exception{
-		assertEquals(6, solution.stringCalculator("1\n ,2,3 "));
+		assertEquals(6, Solution.stringCalculator("1\n ,2,3 "));
 	}
 	
 	@Test
-	public void inputStringWithCommaNewLines() throws Exception{
-		assertEquals(23, solution.stringCalculator("1\n,2\n,3,8,9\n"));
+	public void inputStringWithSingleCustomDelimiter() throws Exception{
+		assertEquals(7, Solution.stringCalculator("//;\n1;2;4"));
 	}
 	
-
+	@Test
+	public void inputStringWithSingleFowareSlashDelimiterNewLine() throws Exception{
+		assertEquals(7, Solution.stringCalculator("///\n1/2/4"));
+	}
+	
+	@Test
+	public void inputStringWithDoubleFowareSlashDelimiterNewLine() throws Exception{
+		assertEquals(7, Solution.stringCalculator("////\n1//2//4"));
+	}
+	
+	@Test
+	public void inputStringWithSingleCustomDelimiter1() throws Exception{
+		assertEquals(7, Solution.stringCalculator("//@\n1@2@4"));
+	}
+	
+	@Test
+	public void inputStringWithSingleCustomDelimiter2() throws Exception{
+		assertEquals(7, Solution.stringCalculator("//,\n1,2,4"));
+	}
+	
+	@Test
+	public void inputStringWithSingleArbitraryCustomDelimiter() throws Exception{
+		assertEquals(7, Solution.stringCalculator("//@@@\n1@@@2@@@4"));
+	}
+	
+	@Test
+	public void inputStringWithSingleCustomDelimiterStar() throws Exception{
+		assertEquals(7, Solution.stringCalculator("//*\n1*2*4"));
+	}
+	
+	@Test
+	public void inputStringWithMutipleArbitraryCustomDelimiterStars() throws Exception{
+		assertEquals(7, Solution.stringCalculator("//***\n1***2***4"));
+	}
+	
+	@Test
+	public void inputStringWithMutipleArbitraryCustomDelimiterCommas() throws Exception{
+		assertEquals(7, Solution.stringCalculator("//,,,\n1,,,2,,,4"));
+	}
+	
+	@Test
+	public void inputStringWithMutipleArbitraryCustomDelimiterLargeNumber() throws Exception{
+		assertEquals(12, Solution.stringCalculator("//@@@,;,%\n1@@@2000;4%7"));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void inputStringWithMutipleArbitraryCustomDelimiterNegativeNumber() throws Exception{
+		assertEquals(12, Solution.stringCalculator("//@@@,;,%\n1@@@-5;4%7"));
+	}
+	
+	@Test
+	public void inputStringWithMutipleArbitraryCustomDelimiterNegativeNumbers() throws Exception{
+		expectedEx.expect(IllegalArgumentException.class);
+	    expectedEx.expectMessage("Negatives not allowed[-5, -9, -12]");
+	    Solution.stringCalculator("//@@@,;,%\n1@@@-5;4%7,-9,-12");
+	}
+	
+	@Test
+	public void inputStringWithMutipleArbitraryCustomDelimiterSpace() throws Exception{
+		assertEquals(14, Solution.stringCalculator("//@@@,;,%\n1 @@@2;4 %7"));
+	}
+	
+	@Test
+	public void inputStringWithMutipleArbitraryCustomDelimiter() throws Exception{
+		assertEquals(14, Solution.stringCalculator("//@@@,;,%\n1@@@2;4%7"));
+	}
 }
